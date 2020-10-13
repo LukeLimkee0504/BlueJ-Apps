3e793bf22f2fd73c1ac4d9eed96946419e9f4806
LukeLimkee0504 <63973595+LukeLimkee0504@users.noreply.github.com>
2020-10-13 12:30:50 +0100

    Create Ticket.java

---
+++ app02-ticketmachine/Ticket.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class Ticket here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Ticket
+{
+    // instance variables - replace the example below with your own
+    private int price;
+    private String location;
+
+    /**
+     * Constructor for objects of class Ticket
+     */
+    public Ticket(String ticketLocation, int ticketPrice)
+    {
+       price = ticketPrice;
+       location = ticketLocation;
+       
+    }
+}
