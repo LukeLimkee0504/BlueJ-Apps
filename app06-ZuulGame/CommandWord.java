1d5df33985a6595605578166c0e4b82c90a6574d
LukeLimkee0504 <63973595+LukeLimkee0504@users.noreply.github.com>
2021-01-09 21:05:35 +0000

    Update CommandWord.java

--- app06-ZuulGame/CommandWord.java
+++ app06-ZuulGame/CommandWord.java
@@ -9,7 +9,7 @@ public enum CommandWord
 {
     // A value for each command word along with its
     // corresponding user interface string.
-    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), LOOK("look");
+    GO("go"), QUIT("quit"), HELP("help"), UNKNOWN("?"), INSPECT("inspect");
     
     // The command string.
     private String commandString;
